4d7389b4c52f5f7e1f47b790fe8d625dbe611d39
limadouglas07 <lima.douglas08@gmail.com>
2025-03-22 16:48:00 -0300

    Update 00_declarando_conjuntos.py

--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -5,4 +5,5 @@ letras = set("abacaxi")
 print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
-print(carros)  # {"gol", "celta", "palio"}
+# {"gol", "celta", "palio"} # uma outra forma de você eliminar elementos duplicados é utilizando os parênteses...
+print(carros)
